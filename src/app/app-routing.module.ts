99c50a7b2b45ca1b03878161974c8fcdb78c0c3b
Sergio A. Mejia <46963589+iSergioMejia@users.noreply.github.com>
2020-04-09 17:35:08 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { VerInmuebleComponent } from './components/inmobiliaria/ver-inmueble/ver-inmueble.component';
-
+import { CrearInmuebleComponent } from './components/inmobiliaria/crear-inmueble/ver-inmueble.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/login'},
   {path: 'login', component: LoginComponent},
-  {path: 'inmobiliaria/ver-inmueble', component: VerInmuebleComponent}
+  {path: 'inmobiliaria/ver-inmueble', component: VerInmuebleComponent},
+  {path: 'inmobiliaria/crear-inmueble', component: CrearInmuebleComponent}
 ];
 
 @NgModule({
