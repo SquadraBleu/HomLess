d1fa6c146d2aa9deee4bd747e0f4207ec59649d8
Julian David <julian_parada@javeriana.edu.co>
2020-04-08 23:47:02 -0500

    Fixed Property firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
   apiKey: "AIzaSyAD24t25q0teECxHE-z2dqCK8jcscDlDAg",
   authDomain: "homless-33d0b.firebaseapp.com",
