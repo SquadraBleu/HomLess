938c39047339cfca5b133952ee698bc493f8c856
Julian David <julian_parada@javeriana.edu.co>
2020-04-09 17:25:54 -0500

    Fixed Lint - " should be '

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAD24t25q0teECxHE-z2dqCK8jcscDlDAg",
-    authDomain: "homless-33d0b.firebaseapp.com",
-    databaseURL: "https://homless-33d0b.firebaseio.com",
-    projectId: "homless-33d0b",
-    storageBucket: "homless-33d0b.appspot.com",
-    messagingSenderId: "569928008974",
-    appId: "1:569928008974:web:4a47ec7fa2d1845d57c35f",
-    measurementId: "G-333QPN3X8E"
+    apiKey: 'AIzaSyAD24t25q0teECxHE-z2dqCK8jcscDlDAg',
+    authDomain: 'homless-33d0b.firebaseapp.com',
+    databaseURL: 'https://homless-33d0b.firebaseio.com',
+    projectId: 'homless-33d0b',
+    storageBucket: 'homless-33d0b.appspot.com',
+    messagingSenderId: '569928008974',
+    appId: '1:569928008974:web:4a47ec7fa2d1845d57c35f',
+    measurementId: 'G-333QPN3X8E'
   }
 };
