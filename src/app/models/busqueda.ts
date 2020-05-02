413050e4adc798705e1035e708d3c66b803ae81b
Sergio Mejía <sergio.mejia@javeriana.edu.co>
2020-05-02 15:41:57 -0500

    Added Busqueda Model and finished Ver Busquedas

---
+++ src/app/models/busqueda.ts
@@ -0,0 +1,22 @@
+import { Tag } from './tag';
+
+export class Busqueda {
+    constructor(
+        public IDBusqueda: string,
+        public Descripcion: string,
+        public TipoInmueble: string,
+        public AreaMinima: number,
+        public AreaMaxima: number,
+        public NHabitaciones: number,
+        public NBanos: number,
+        public Zona: string,
+        public Localidad: string,
+        public PrecioMinVenta: number,
+        public PrecioMaxVenta: number,
+        public PrecioMinArriendo: number,
+        public PrecioMaxArriendo: number,
+        public SiNotificacion: boolean,
+        public IDCliente: string,
+        public Tags: Tag[]
+    ){}
+}
