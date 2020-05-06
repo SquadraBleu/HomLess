66f25f1bce7421a4e66d9ec07f602022ddf7b30a
Mateo Florido <mateoflosan@gmail.com>
2020-05-06 10:05:55 -0500

    Algolia Powered Search

---
+++ functions/src/index.ts
@@ -0,0 +1,47 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+const env = functions.config();
+
+import * as algoliasearch from 'algoliasearch'
+
+//Init Algolia Pipe Client
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex('inmuebles_search');
+
+exports.indexInmuebles = functions.firestore
+  .document('Inmuebles/{UID}')
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const objectId = snap.id;
+    //Push data to Algolia Index
+    return index.addObject({
+      objectId,
+      ...data
+    });
+  });
+
+exports.unindexInmuebles = functions.firestore
+  .document('Inmuebles/{UID}')
+  .onDelete((snap, context) => {
+    const data = snap.data();
+    const objectId = snap.id;
+    //Remove data to Algolia Index
+    return index.deleteObject(objectId);
+  });
+exports.updateindexInmuebles = functions.firestore
+  .document('Inmuebles/{UID}')
+  .onUpdate((snap, context) => {
+    const data = snap.after;
+    const objectId = data.id;
+    return index.addObject({
+      objectId,
+      ...data
+    });
+  });
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
