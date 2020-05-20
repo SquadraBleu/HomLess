38994d253d7e3122fba0b64114f4ee921b0249ca
Daniela <dcortesantonio@gmail.com>
2020-05-19 19:51:11 -0500

    Activate Searches Fixed

--- src/app/components/client/ver-busquedas/ver-busquedas.component.spec.ts
+++ src/app/components/client/ver-busquedas/ver-busquedas.component.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { VerBusquedasComponent } from './ver-busquedas.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {VerBusquedasComponent} from './ver-busquedas.component';
+import {By} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
 
 describe('VerBusquedasComponent', () => {
-  let component: VerBusquedasComponent;
-  let fixture: ComponentFixture<VerBusquedasComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VerBusquedasComponent ]
-    })
-    .compileComponents();
-  }));
-
+  // tslint:disable-next-line:prefer-const
+  let authServiceSpy;
+  // tslint:disable-next-line:prefer-const
+  let routerSpy;
   beforeEach(() => {
-    fixture = TestBed.createComponent(VerBusquedasComponent);
-    component = fixture.componentInstance;
+    TestBed.resetTestingModule();
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
+      declarations: [VerBusquedasComponent],
+      providers: [{provide: AuthService, useValue: authServiceSpy},
+        {provide: Router, useValue: routerSpy},
+      ]
+    }).compileComponents();
+  });
+  /*
+    it('should activate notifications', (() => {
+      const fixture = TestBed.createComponent(VerBusquedasComponent);
+      const comp = fixture.componentInstance;
+      const deb = fixture.debugElement.query(By.css('form'));
+      fixture.detectChanges();
+      spyOn(comp, 'activarBusqueda').withArgs(1);
+      expect(comp.busquedas[1].SiNotificacion).toBeTrue();
+    }));
+  */
+  it('should get Searches', (() => {
+    const fixture = TestBed.createComponent(VerBusquedasComponent);
+    const comp = fixture.componentInstance;
+    const deb = fixture.debugElement.query(By.css('form'));
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    spyOn(comp, 'ngOnInit');
+    // @ts-ignore
+    expect(comp.busquedas.length()).toEqual(5);
+  }));
 });
