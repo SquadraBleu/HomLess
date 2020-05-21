0dcbf4cd04939f48a9511a8bc3a2282f9fd5c5c8
Daniela <dcortesantonio@gmail.com>
2020-05-20 22:52:04 -0500

    Test of Ver-Busquedas

--- src/app/components/client/ver-busquedas/ver-busquedas.component.spec.ts
+++ src/app/components/client/ver-busquedas/ver-busquedas.component.spec.ts
@@ -1,25 +1,62 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {VerBusquedasComponent} from './ver-busquedas.component';
 import {By} from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AuthService} from '../../../services/auth.service';
-import {Router} from '@angular/router';
-import {SearchComponent} from "../../public/search/search.component";
+import {ActivatedRoute, Router} from '@angular/router';
+import {BusquedaService} from '../../../services/busqueda.service';
+import {InmuebleServiceService} from '../../../services/inmueble-service.service';
+import {Observable, Subject} from 'rxjs';
+import {Tag} from '../../../models/tag';
+import {Busqueda} from 'src/app/models/busqueda';
 
 describe('VerBusquedasComponent', () => {
   // tslint:disable-next-line:prefer-const
-  let authServiceSpy;
+  let routerSpy = {
+    navigate: jasmine.createSpy('navigate')
+  };
   // tslint:disable-next-line:prefer-const
-  let routerSpy;
+  let router: Router;
   beforeEach(() => {
+    let busquedaServiceSpy: Partial<BusquedaService>;
+    busquedaServiceSpy = {
+      getBusquedas(): Observable<any[]> {
+        const observable$ = new Subject<Busqueda[]>();
+        observable$.next(
+          [new Busqueda('', 'Casita', '', 0, 0, 0, 0, '', '', 0, 0, 0, 0, false, '', [], '', null)
+          ]);
+        return observable$.asObservable();
+      }
+    };
+    let inmuebleServiceSpy: Partial<InmuebleServiceService>;
+    inmuebleServiceSpy = {
+      getTags(): Observable<any[]> {
+        const observable$ = new Subject<Tag[]>();
+        observable$.next([new Tag('Parqueadero', [], '')]);
+        return  observable$.asObservable();
+      }
+    };
     TestBed.resetTestingModule();
     TestBed.configureTestingModule({
-      imports: [FormsModule, ReactiveFormsModule],
+      imports: [FormsModule, ReactiveFormsModule, RouterTestingModule.withRoutes([])],
       declarations: [VerBusquedasComponent],
-      providers: [{provide: AuthService, useValue: authServiceSpy},
+      providers: [
+        {provide: BusquedaService, useValue: busquedaServiceSpy},
+        {provide: InmuebleServiceService, useValue: inmuebleServiceSpy},
         {provide: Router, useValue: routerSpy},
       ]
     }).compileComponents();
+
+
+
+  });
+  it('should click link', () => {
+    const fixture = TestBed.createComponent(VerBusquedasComponent);
+    const component = fixture.componentInstance;
+    router = TestBed.get(Router);
+    const navigateSpy = spyOn( router, 'navigate');
+    component.activarBusqueda(0);
+    expect(navigateSpy).toHaveBeenCalledWith('cliente/ver-busqueda/' + component.id);
   });
 
 });
